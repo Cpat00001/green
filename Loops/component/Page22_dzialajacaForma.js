770baf3c9a3188cf2ebedbed4e4f6a63c9b327dd
Patryk Chyla <patryk.chyla@ad.hubwise.co.uk>
2019-09-06 11:40:15 +0100

    changes

--- Loops/component/Page22_dzialajacaForma.js
+++ Loops/component/Page22_dzialajacaForma.js
@@ -15,8 +15,8 @@ const { Option } = Select;
          super(props);
          this.state={
              size: 'large',
-             gender:['-','Male','Female','Other'],
-             maritalStatus:['-','Single','Married','Civil partnership','Divorced','Widowed'],
+             gender:['Male','Female','Other'],
+             maritalStatus:['Single','Married','Civil partnership','Divorced','Widowed'],
              adviser:'',
              title:'',
              fName:'',
@@ -38,9 +38,7 @@ const { Option } = Select;
              mobilePhone:'',
              email:'',
              natification:false,
-             spouse:false,
-             submitButton: false,
-             label: false
+             spouse:false
     
 
          }
@@ -70,78 +68,28 @@ const { Option } = Select;
      handleSubmit(event){
         event.preventDefault();
         console.log(this.state)
-        const {fName,sName,email,errors} = this.state;
-        
-        //additional email validation
-        // this.checkEmail();
-
-        // function checkEmail(email){
-        //     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        //     return regex.test(email);
-        // }
-
-        //check if not empty
-        // if(fName === ''){
-        //     this.setState({errors:{name:'Name is required'}})
-        //     return;
-        // }
-        // if(sName === ''){
-        //     this.setState({errors:{name:'Surname is required'}})
-        //     return;
-        // }
-        // if(email ===''){
-        //     this.setState({errors:{email:'Email is required'}})
-        //     return;
-        // }
-         console.log('SUBMIT CLICKED && errors')
+         console.log('SUBMIT CLICKED')
      }
-     
      handleChange(event){
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
         const checked = event.target.checked
         this.setState({ [name]:value})
-
-        
         
      }
      handleChangeDate(date,dateString){
         this.setState({ date: date })
      }
-    //  handleBlur(e){
-    //     console.log(this, e.target)
-    handleBlur = (e) =>{
-        console.log(e.target.value)
-        console.log('this is',this)
-         
-         if(isNaN(e.target.value)){
-            this.setState({label:true})
-            console.log(this.state.label)
-            setTimeout(()=>{this.setState({label:false})},2000)
-        }
-     }
-     
-    render() {
-        //display error label
-        let label;
-        if(this.state.label === true){
-            label = <label htmlFor="error" style={{color:'red'}}>Sorry only number allowed</label>
-        }
 
+
+    render() {
         // console.log('rzuc advisors',this.props.advisors)
         // console.log(this.props.country)
         // console.log(this.props.nation)
         // console.log(this.props.tax)
         //console.log(this.props)
-        const {fName,sName,address1,town,country,date,tax,gen,nin,email} = this.state;
-        //make submit button available if => fName,sName,email are fulfilled
-        let button;
-        if(fName !=='' && sName !== '' && address1 !== '' && town !== '' && country !== '' && date !== '' && tax !=='' && gen !== '' && nin !=='' && email !== ''){
-            button = <Button size={this.state.size} style={{backgroundColor:'green',color:'white'}} htmlType="submit">Register</Button> 
-        }else{
-            button = <Button size={this.state.size} style={{backgroundColor:'grey',color:'white'}}>Disable</Button>
-        }
+
         return (
             
                 <form onSubmit={this.handleSubmit}>
@@ -234,7 +182,7 @@ const { Option } = Select;
                             <Row style={{marginTop:'5%'}}>
                                 <Col span={6}><p className='txt5'>Title</p></Col>
                                 <Col span={12}>
-                                    <select className='dropdownList22Req' name='title' value={this.state.title} onChange={this.handleChange} >
+                                    <select className='dropdownList22' name='title' value={this.state.title} onChange={this.handleChange} >
                                         {this.props.title.map((title,index)=>(
                                             <option key={index} value={title.value}>{title.value}</option>
                                         ))}
@@ -245,17 +193,17 @@ const { Option } = Select;
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>First Name</p></Col>
-                                <Col span={12}><Input name='fName' value={this.state.fName} onChange={this.handleChange} placeholder='required field' className='input2'/></Col>
+                                <Col span={12}><Input name='fName' value={this.state.fName} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>Middle Name</p></Col>
-                                <Col span={12}><Input name='mName' value={this.state.mName} onChange={this.handleChange} className='input1'/></Col>
+                                <Col span={12}><Input name='mName' value={this.state.mName} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>Surname</p></Col>
-                                <Col span={12}><Input name='sName' value={this.state.sName} onChange={this.handleChange} placeholder='required field...' className='input2'/></Col>
+                                <Col span={12}><Input name='sName' value={this.state.sName} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                         </div>
@@ -264,28 +212,28 @@ const { Option } = Select;
                             <div className='div2'><Icon type="pushpin" className='iconLabel1'/><p className='txt4'>Adress details</p></div>
                             <Row style={{marginTop:'5%'}}>
                                 <Col span={6}><p className='txt5'>Address 1</p></Col>
-                                <Col span={12}><Input name='address1'value={this.state.address1} onChange={this.handleChange} className='input2' placeholder='required field...'/></Col>
+                                <Col span={12}><Input name='address1'value={this.state.address1} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>Address 2</p></Col>
-                                <Col span={12}><Input name='address2' value={this.state.address2} onChange={this.handleChange} className='input1' placeholder='required field...'/></Col>
+                                <Col span={12}><Input name='address2' value={this.state.address2} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>Town/City</p></Col>
-                                <Col span={12}><Input name='town' value={this.state.town} onChange={this.handleChange} className='input2'placeholder='required field...'/></Col>
+                                <Col span={12}><Input name='town' value={this.state.town} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>County/Region</p></Col>
-                                <Col span={12}><Input name='region' value={this.state.region} onChange={this.handleChange} className='input1'/></Col>
+                                <Col span={12}><Input name='region' value={this.state.region} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>Country</p></Col>
                                 <Col span={12}>
-                                <select style={{width:'100%'}} name='country' value={this.state.country} onChange={this.handleChange} className='dropdownList22Req'>
+                                <select style={{width:'100%'}} name='country' value={this.state.country} onChange={this.handleChange} className='dropdownList22'>
                                     {this.props.country.map((c,index)=>(
                                         <option key={index} value={c.Title}>{c.Title}</option>
                                     ))}
@@ -304,7 +252,7 @@ const { Option } = Select;
                             <Row style={{marginTop:'5%'}}>
                                 <Col span={6}><p className='txt5'>Date of Birth</p></Col>
                                 {/* mozliwe ze w DatePicker tez bede musial dodac onChange + name to handle multiple inputChanges */}
-                                <Col span={12}><DatePicker style={{width:'100%'}} name='date' value={this.state.date} onChange={this.handleChangeDate} className='input2'/></Col>  
+                                <Col span={12}><DatePicker style={{width:'100%'}} name='date' value={this.state.date} onChange={this.handleChangeDate}/></Col>  
                                 {/* <Col span={12}><DatePicker style={{width:'100%'}} name='date' value={this.state.date} onChange={this.handleChange}/></Col>   */}
 
                                 <Col span={6}></Col>
@@ -312,7 +260,7 @@ const { Option } = Select;
                             <Row>
                                 <Col span={6}><p className='txt5'>Nationality</p></Col>
                                 <Col span={12}>
-                                    <select className='dropdownList22' name='nationality' value={this.state.nationality} onChange={this.handleChange} className='dropdownList22Req'>
+                                    <select className='dropdownList22' name='nationality' value={this.state.nationality} onChange={this.handleChange}>
                                         {this.props.nation.map((nat,index)=>(
                                             <option key={index} value={nat.nationalityTitle}>{nat.nationalityTitle}</option>
                                         ))}
@@ -323,7 +271,7 @@ const { Option } = Select;
                             <Row>
                                 <Col span={6}><p className='txt5'>Country of Taxation</p></Col>
                                 <Col span={12}>
-                                    <select className='dropdownList22' name='tax' value={this.state.tax} onChange={this.handleChange} className='dropdownList22Req'>
+                                    <select className='dropdownList22' name='tax' value={this.state.tax} onChange={this.handleChange}>
                                         {this.props.tax.map((tax,index)=>(
                                             <option key={index} value={tax.country}>{tax.country}</option>
                                         ))}
@@ -335,7 +283,7 @@ const { Option } = Select;
                                 <Col span={6}><p className='txt5'>Gender</p></Col>
                                 {/* mozliwe ze w DatePicker tez bede musial dodac onChange + name to handle multiple inputChanges */}
                                 <Col span={12}>
-                                    <select name='gen' value={this.state.gen} onChange={this.handleChange} className='dropdownList22Req'>
+                                    <select name='gen' value={this.state.gen} onChange={this.handleChange} className='dropdownList22'>
                                         {this.state.gender.map((gen,index)=>(
                                             <option key={index}>{gen}</option>
                                          ))
@@ -346,7 +294,7 @@ const { Option } = Select;
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>NIN Number</p></Col>
-                                <Col span={12}><Input name='nin' value={this.state.nin} onChange={this.handleChange} className='input2'/></Col>
+                                <Col span={12}><Input name='nin' value={this.state.nin} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
@@ -364,32 +312,19 @@ const { Option } = Select;
                         {/* contact detail */}
                         <div className='div3'>
                             <div className='div2'><Icon type="phone" className='iconLabel1'/><p className='txt4'>Contact Numbers</p></div>
-                            {/* <Row>
-                                <Col span={6}></Col>
-                                <Col span={6}>{label}</Col>
-                                <Col span={6}></Col>                                
-                            </Row> */}
-                            <Row>
-                                <Col span={6}></Col>
-                                <Col span={12}>
-                                {label}
-                                </Col>
-                                <Col span={6}></Col>
-                            </Row>
                             <Row style={{marginTop:'5%'}}>
                                 <Col span={6}><p className='txt5'>Contact Phone</p></Col>
-                                <Col span={12}><Input name='contactPhone' type='text' value={this.state.contactPhone} onChange={this.handleChange} onBlur={this.handleBlur} className='input1'/>
-                                </Col>
+                                <Col span={12}><Input name='contactPhone' value={this.state.contactPhone} onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>Daytime Phone</p></Col>
-                                <Col span={12}><Input name='daytimePhone' type='text' value={this.state.daytimePhone}onChange={this.handleChange} className='input1' onBlur={this.handleBlur}/></Col>
+                                <Col span={12}><Input name='daytimePhone' value={this.state.daytimePhone}onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                             <Row>
                                 <Col span={6}><p className='txt5'>Mobile Phone</p></Col>
-                                <Col span={12}><Input name='mobilePhone' type='text' value={this.state.mobilePhone}onChange={this.handleChange} className='input1' onBlur={this.handleBlur}/></Col>
+                                <Col span={12}><Input name='mobilePhone' value={this.state.mobilePhone}onChange={this.handleChange}/></Col>
                                 <Col span={6}></Col>
                             </Row>
                         </div>
@@ -398,14 +333,13 @@ const { Option } = Select;
                             <div className='div2'><Icon type="mail" className='iconLabel1'/><p className='txt4'>Email Details</p></div>
                         <Row style={{marginTop:'5%'}}>
                             <Col span={6}><p className='txt5'>Email</p></Col>
-                            <Col span={12}><Input name='email' type='email' value={this.state.email}onChange={this.handleChange} placeholder='input required...' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
-                            className='input2'/></Col>
+                            <Col span={12}><Input name='email' value={this.state.email}onChange={this.handleChange}/></Col>
                             <Col span={6}></Col>
                         </Row>
                         <Row>
                             <Col span={6}><p className='txt5'>Notification</p></Col>
                             {/* mozliwe ze bede musial dac onChange na tym checkboxie */}
-                            <Col span={12}><Checkbox style={{float:'left'}} name='natification' value={this.state.natification} type="checkbox" onChange={this.handleChange} className='input1'/></Col> 
+                            <Col span={12}><Checkbox style={{float:'left'}} name='natification' value={this.state.natification} type="checkbox" onChange={this.handleChange}/></Col> 
                             <Col span={6}></Col>
                         </Row>
                         </div>
@@ -417,13 +351,13 @@ const { Option } = Select;
                 <Col xs={10} sm={10} md={10} xl={10}></Col>
                 <Col xs={10} sm={10} md={5} xl={5}>
                     <div className='divButtons'>
-                        
-                        {button}
-
+                        <Button size={this.state.size} style={{backgroundColor:'green'}} htmlType="submit">
+                            Register
+                       </Button> 
+                        {/* <input type="submit" value="Submit" /> */}
                         <Button type="danger" size={this.state.size} style={{backgroundColor:'red',color:'white'}}>
                             Cancel
                         </Button>
-                       
                     </div>
                 </Col>
                 <Col xs={4} sm={4} md={9} xl={9}></Col>
@@ -450,6 +384,6 @@ const mapStateToProps = state => {
         title: state.form22.title
     }
 }
-export default connect(mapStateToProps,{getAdvisors,getCountry,getNation,getTax,getTitle})  (Page22);
+export default connect(mapStateToProps,{getAdvisors,getCountry,getNation,getTax,getTitle}) (Page22);
 
 
